7f7b28979334f25a9da75e3195e18c6563f0cd4d
Pierre Fajardo <pierrefaja@gmail.com>
2018-09-21 15:47:40 +0200

    quete java

---
+++ Classroom.java
@@ -0,0 +1,11 @@
+public class Classroom {
+   
+  public  static void main (String [] arg){
+
+  
+  Wilder jean = new Wilder ("Jean Claude " , true);
+     System.out.println(jean.whoAmI());
+  Wilder Truc = new Wilder ("Truc " , false );
+     System.out.println (Truc.whoAmI());
+  }
+}
